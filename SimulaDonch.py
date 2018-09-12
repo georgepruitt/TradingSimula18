1ae35ffb5442e1d1c0caf70449faa1066e2941fa
George Pruitt <george.p.pruitt@gmail.com>
2018-09-12 15:15:33 -0400

    Added SimulaDonch
    Cleaned up some problems

--- SimulaDonch.py
+++ SimulaDonch.py
@@ -6,6 +6,7 @@
 #Import Section - inlcude functions, classes, variables
 #from external modules
 #--------------------------------------------------------------------------------
+
 from getData import getData
 from equityDataClass import equityClass
 from tradeClass import tradeInfo
@@ -56,44 +57,31 @@ def bookTrade(entryOrExit,lOrS,price,date,tradeName,shares):
         if lOrS ==-1:
             mp -= 1
             trades = tradeInfo('sell',date,tradeName,entryPrice[-1],shares,1)
-
     return(profit,curShares,trades)
 
 dataClassList = list()
-
 
 def main():
     pass
 
 if __name__ == '__main__':
-
-    marketMonitorList = list()
-    masterDateList = list()
-    masterDateGlob = list()
-    marketPosition = list()
-    entryPrice= list()
-    entryQuant= list()
-    exitQuant= list()
-    trueRanges= list()
-    marketDataInc= list()
-    myBPVList = list()
-    myComNameList = list()
-    myMinMoveList= list()
+    marketMonitorList,masterDateList,masterDateGlob,entryPrice = ([] for i in range(4))
+    entryQuant,exitQuant,trueRanges,myBPVList = ([] for i in range(4))
+    myComNameList,myMinMoveList,systemMarketList = ([] for i in range(3))
     portManager = portManagerClass()
     marketList = getData()
-    marketDataInc.append(0)
-    curShares = 0
-    commission = 50
-    systemMarketList = list()
     portfolio = portfolioClass()
     numMarkets = len(marketList)
+
+    curShares = 0
+    commission = 50
+
     for i in range(0,numMarkets):
         systemMarkTracker = systemMarkTrackerClass()
         equity = equityClass()
         systemMarkTracker.setSysMarkTrackingData(marketList[i])
         systemMarkTracker.setSysMarkTrackingEquity(equity)
         marketMonitorList.append(systemMarkTracker)
-        marketDataInc.append(0)
         myBPV,myComName,myMinMove= getDataAtribs(marketMonitorList[i].marketData)
         myBPVList.append(myBPV)
         myComNameList.append(myComName)
@@ -106,14 +94,10 @@ if __name__ == '__main__':
         masterDateGlob += marketMonitorList[i].marketData.date
     masterDateList = removeDuplicates(masterDateGlob)
     masterDateList = sorted(masterDateList)
-    x= 1
-    jCnt = 0
-    k=5
     barsSinceEntry = 0
     portEquItm = 0
     for i in range(len(masterDateList)-700,len(masterDateList)):
         portManager.portDate.append(masterDateList[i])
-        kCnt = 0
         dailyPortCombEqu = 0
         for j in range(0,numMarkets):
             myDate,myOpen,myHigh,myLow,myClose,myVolume,myOpInt = setDataLists(marketMonitorList[j].marketData)
@@ -140,7 +124,6 @@ if __name__ == '__main__':
                 entryQuant= marketMonitorList[j].entryQuant
                 curShares = marketMonitorList[j].curShares
                 cumuProfit = marketMonitorList[j].cumuProfit
-
                 barsSinceEntry = marketMonitorList[j].barsSinceEntry
                 cumuProfit = marketMonitorList[j].cumuProfit
                 if k > 5:
@@ -155,7 +138,6 @@ if __name__ == '__main__':
                         profit,curShares,trades = bookTrade(1,1,price,myDate[k],tradeName,numShares)
                         barsSinceEntry = 1
                         marketMonitorList[j].setSysMarkTrackingInfo(tradeName,cumuProfit,mp,barsSinceEntry,curShares)
-
                         marketMonitorList[j].tradesList.append(trades)
                     if myLow[k] <= lowest(myLow,40,k,1) and barsSinceEntry > 1 and mp !=-1:
                         price = min(myOpen[k],lowest(myLow,40,k,1))
@@ -163,8 +145,6 @@ if __name__ == '__main__':
                             profit,curShares,trades = bookTrade(-1,0,price,myDate[k],"RevLongLiq",curShares)
                             marketMonitorList[j].tradesList.append(trades)
                             todaysCTE = profit
-
-
                         tradeName = "Test S"
                         numShares = 4
                         profit,curShares,trades = bookTrade(1,-1,price,myDate[k],tradeName,numShares)
